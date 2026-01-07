525f21bf462734a9c6977b756c8c0ef554d2b1dd
freesiapro <freesiapro@outlook.com>
2026-01-07 16:18:53 +0800

    initial commit

---
+++ exercises/09_strings/strings1.rs
@@ -0,0 +1,11 @@
+// TODO: Fix the compiler error without changing the function signature.
+fn current_favorite_color() -> String {
+    // String::from("blue")
+    // "blue".to_string()
+    "blue".into()
+}
+
+fn main() {
+    let answer = current_favorite_color();
+    println!("My current favorite color is {answer}");
+}
